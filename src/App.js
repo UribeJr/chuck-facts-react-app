f403207fc739c055498ae5d7e69ca31fa2a54c19
Uribe <euribe@MacBook-Pro.local>
2021-04-20 00:51:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	state = {facts:''};
+	componentDidMount() {
+		this.fetchFact();
+	}
+	fetchFact = () => {
+			axios.get('https://api.chucknorris.io/jokes/random')
+			.then((resp) => {
+				const facts = resp.data.value;
+				
+				this.setState({ facts });
+			})
+			.catch((err) => {
+				console.log('ooops something broke!',err);
+			});
+		}
+
+	render() {
+		const {facts} = this.state;
+		return (
+			<div className="app">
+				<div className="card">
+				<h1 className = "heading">{facts}</h1>
+				<button className="button" onClick={this.fetchFact}>
+				<span>ROUNDHOUSE!</span>
+				</button>
+				</div>
+			</div>
+			)
+	}
 }
 
 export default App;
